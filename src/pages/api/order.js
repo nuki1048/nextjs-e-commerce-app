a1027c2e15ab14c0b2f36100719a32c5ff9f15c9
Yur1 <nik456618@gmail.com>
2023-07-16 18:15:43 +0300

    Changed API route
    Made some changes in the API route. The main thing is an email template for the client and owner of the site. Now if some people buy some products and type in the checkout form real email it receives to email version of the order with some info about an order.

--- src/pages/api/order.js
+++ src/pages/api/order.js
@@ -1,14 +1,14 @@
-import ThanksfulModal from "@/components/thanksful-modal/thanksful-modal";
 import { connectToDatabase } from "@/lib/db";
 import { sendMessage } from "@/lib/email";
 import { orderNumsGenerator } from "@/lib/order";
 import { deleteCookie } from "cookies-next";
 import { render } from "@react-email/render";
-import EmailMessage from "@/components/emailMessage/emailMessage";
+import Email from "@/emails/order";
 
 async function handler(req, res) {
   if (req.method !== "POST") {
-    return res.status(405).send("Method Not Allowed");
+    res.status(405).json({ message: "Method Not Allowed" });
+    return;
   }
   const { products, billingDetails, deliveryInfo, paymentMethod, summary } =
     req.body;
@@ -21,8 +21,9 @@ async function handler(req, res) {
   //     summary: "$1000",
   //   };
 
-  if (products.length === 0) {
-    return res.status(400).send("We can't apply order without products");
+  if (!products || products.length === 0) {
+    res.status(400).json({ message: "We can't apply order without products" });
+    return;
   }
 
   if (
@@ -31,7 +32,9 @@ async function handler(req, res) {
     !billingDetails.telephone ||
     !billingDetails.email
   ) {
-    return res.status(400).send("We can't apply order without billing details");
+    return res
+      .status(400)
+      .json({ message: "We can't apply order without billing details" });
   }
 
   if (
@@ -43,21 +46,26 @@ async function handler(req, res) {
   ) {
     return res
       .status(400)
-      .send("We can't apply order without delivery details");
+      .json({ message: "We can't apply order without delivery details" });
   }
 
   if (!paymentMethod) {
-    return res.status(400).send("We can't apply order without payment method");
+    return res
+      .status(400)
+      .json({ message: "We can't apply order without payment method" });
   }
 
   let client;
   try {
     client = await connectToDatabase();
   } catch (error) {
-    res.status(400).send("We can't connect to database");
+    res.status(400).json({ message: "We can't connect to database" });
   }
 
   const orderNumber = orderNumsGenerator();
+  const dateOfOrder = new Date().toISOString();
+  console.log(dateOfOrder);
+
   const order = {
     orderNumber,
     products,
@@ -65,30 +73,75 @@ async function handler(req, res) {
     deliveryInfo,
     paymentMethod,
     summary,
+    orderDate: dateOfOrder,
   };
 
-  const db = client.db();
+  let db;
+
+  try {
+    db = await client.db();
+  } catch (error) {
+    res.status(400).json({ message: "We can't connect to database" });
+  }
+
   const collection = db.collection("orders");
   let response;
   try {
     response = await collection.insertOne(order);
   } catch (error) {
     client.close();
-    return res.status(400).send("We can't apply order");
+    return res.status(400).json({ message: "We can't apply order" });
   }
 
   deleteCookie("cart", { req, res });
 
+  res
+    .status(201)
+    .json({ order: response, message: "Order added successfully" });
+
   await sendMessage({
+    from: '"Audiophile 👻" <audiophile@gmail.com>',
+    to: "nik456618@gmail.com",
+    subject: "New Order ✔",
+    text: `
+      New Order \n 
+
+      Order Info: \n
+      Order Number: ${orderNumber},\n
+      Email: ${billingDetails.email},\n
+      Full Name: ${billingDetails.name},\n
+      Telephone: ${billingDetails.telephone},\n
+
+      ${products.map(
+        (item) => `
+      Product: \n
+      Name: ${item.name},\n
+      Count: ${item.count},\n
+      Price: ${item.price},\n
+      `
+      )}\n
+
+      Order summary: ${
+        summary.totalWithTaxAndDelivery
+      }(without taxes and delivery ${summary.totalWithoutTaxesAndDelivery});
+      `,
+  });
+
+  return await sendMessage({
     from: '"Audiophile 👻" <audiophile@gmail.com>',
     to: billingDetails.email,
     subject: "Order ✔",
     text: "Order Details",
-    html: render(EmailMessage({ cartItems: order.products, total: "5000" })),
+    html: render(
+      Email({
+        products: [...products],
+        summary,
+        deliveryInfo,
+        name: billingDetails.name,
+        orderDate: dateOfOrder,
+        orderNumber,
+      })
+    ),
   });
-
-  return res
-    .status(201)
-    .json({ order: response, message: "Order added successfully" });
 }
 export default handler;
